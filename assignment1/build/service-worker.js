53233ac59d8ddc40b56e29d50ab2ac6458686e35
Lidor Cohen <LidorC145@Gmail.com>
2020-05-08 05:00:16 +0300

    fix for build weather app (homepage  variable didn't update)

--- assignment1/build/service-worker.js
+++ assignment1/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FullStackHw3/build/precache-manifest.bede8a6746bcf10e452539abcc07255c.js"
+  "/FullStackHw3/assignment1/build/precache-manifest.7a544eaab9b769b5e4eecb9237fe2f56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FullStackHw3/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FullStackHw3/assignment1/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
